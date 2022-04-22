5daadb6a6fbfcfd7686269b41b4a1432c445730e
gusLopezC <guslopezcallejas@gmail.com>
2022-04-22 10:10:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "Hey there I am a PHP file from Kubernetes Master Testing Jenkins Build";
+echo "<br> We are Live";
+echo "<br> Job was Started by Github Webhook";
+echo "<br> One more new line added just because previous build failed";
+echo "<br> All those builds failed just because directory name had a space";
+echo "<br> I did One more mistake so added one more line";
+echo "<br> I promise this is Seriously the last mistake ever";
+echo "<br> Testing Job DSL so just written this line to push to Github and start the job using the webhook";
+echo "<br> Minor Mistake Job DSL Build Failed before";
+echo "<br> Finally this Webpage has been deployed by Jenkins DSL";
+echo "<br> All 4 Jobs are working fine and DSL worked";
+echo "<br> Testing Manual to Auto change on CI-Pipeline Devtron";
+?>
